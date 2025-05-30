bc6ef9e2855052d09b1204a6353d56dd7d535fcb
nmetens <metens@pdx.edu>
2025-05-30 11:07:34 -0700

    Learning R syntax with functions and control flow

--- main.r
+++ main.r
@@ -1,3 +1,45 @@
-
 # Comments Here
-cat("Hello Wolrd\n")
+cat("Hello World\n")
+
+x <- 20
+y <- 50
+z <- 6
+
+cat(x, y, z, "\n")
+
+a = (x * z) - y
+
+cat("The result =", a, "\n")
+
+vals <- c(x, y, z, a)
+
+vals
+
+for (val in vals) {
+    cat(val, " ")
+}
+
+cat("\n")
+
+hello <- 'Hello World #2' # This is the string, can be "" or ''
+hello
+
+add_nums <- function(a, b) {
+    return (a + b)
+}
+
+add_nums(x, z)
+
+factorial <- function(num) {
+    if (num == 0) {
+        return (1)
+    }
+    num = factorial(num - 1) * num
+    return (num)
+}
+
+num = 9 
+fact = factorial(num)
+
+sprintf("%s! = %s", num, fact)
+
